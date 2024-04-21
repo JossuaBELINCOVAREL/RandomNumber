6a555c190149c3199fc8ba0ceb41817e2e46947c
Joko <belinjossua@gmail.com>
2024-04-21 18:57:50 +0200

    Add Program

---
+++ RandomNumber/RandomNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int j = 0; j++;
+        Random rnd = new Random();
+        while (true)
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine(rnd.Next(1,100));
+        }
+    }
+}
